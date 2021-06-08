fd8b41ddd34b0438f7ffb8e09ab39e9c07c6fd6c
Nellie Crocker <crocker.nellie@gmail.com>
2021-06-08 19:29:04 -0400

    first commit

--- warmups/arrays.js
+++ warmups/arrays.js
@@ -1,1 +1,41 @@
 //Day 1 blue badge
+
+var foods = ["apple", "orange", "rice", "bread", "tofu"]
+//console log each item in reverse order
+for (let i=foods.length - 1; i >- 1; i--){
+    if (i % 2 == 1){
+    let word = foods[i]
+    let first_letter = word[0].toUpperCase()
+    let rest = word.slice(1)
+    console.log(first_letter + rest);
+}}
+
+
+
+let allergies = ["gluten", "soy"]
+
+for (let food of foods){
+    console.log(
+        food,
+        foodLength(food),
+        foodAllergy(food))
+        //assigns a string based on the function returning true / false
+        let longWordResult = foodLength(food) ? "" : "not "
+        //formatting the output
+        let response = `${food} is ${longWordResult}a long word, and ${foodAllergy(food).toLocaleLowerCase()} it with your allergies`
+        console.log(response);
+}
+
+function foodLength(food){
+    return food.length > 4
+}
+
+function foodAllergy (food){
+    allergyMap = {
+        bread :"gluten",
+        tofu :"soy"
+    }
+    if (allergies.includes(allergyMap[food])) return "You can't eat"
+    return "You can eat"
+
+}
